0f55b396d7ac6aa5e0ceb854ecbf2afd32862ed1
int3l <int3l@users.noreply.github.com>
2020-11-07 17:02:50 +0200

    Fix virtual env folder name

--- otp.ps1
+++ otp.ps1
@@ -1,7 +1,7 @@
 # Set-PSReadlineOption -HistorySaveStyle SaveNothing
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
 cd $scriptPath
-. .\env\Scripts\activate
+. .\venv\Scripts\activate
 python .\main.py
 deactivate
 $host.ui.RawUI.ReadKey("NoEcho,IncludeKeyDown")
